4f39e2d3c91ce45591318e06d2ffeb3bcd091efe
adam goldsmith <adam.goldsmith0@gmail.com>
2023-02-07 09:49:24 +0000

    modifiying the action

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const github = require('@actions/github');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const testStatus = core.getInput('test-status');
+  if(testStatus === False){
+    console.log("The Test failed!");
+    return;
+  }
+  console.log("the Test passed!")
 } catch (error) {
   core.setFailed(error.message);
 }
