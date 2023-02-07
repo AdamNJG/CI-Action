35a41f18111d9e498c177dc838ab8778b86e30f3
adam goldsmith <adam.goldsmith0@gmail.com>
2023-02-07 09:51:45 +0000

    recompiled the action!

--- dist/index.js
+++ dist/index.js
@@ -9779,13 +9779,12 @@ const github = __nccwpck_require__(6738);
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const testStatus = core.getInput('test-status');
+  if(testStatus === False){
+    console.log("The Test failed!");
+    return;
+  }
+  console.log("the Test passed!")
 } catch (error) {
   core.setFailed(error.message);
 }
